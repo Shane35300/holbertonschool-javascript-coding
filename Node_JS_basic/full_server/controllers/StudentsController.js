2e10e7121fa200110f6cb04a6f2160c9b3c14e56
shane <6874@holbertonstudents.com>
2024-02-02 14:28:15 +0100

    correcting task 8

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,15 +1,14 @@
-const path = require('path');
 const readDatabase = require('../utils');
 
 class StudentsController {
   static getAllStudents(request, response) {
-    readDatabase(path.join(__dirname, '../../database.csv'))
+    readDatabase('./database.csv')
       .then((data) => {
         const result = `${data.sentence0}\n${data.sentence2}\n${data.sentence3}`;
         response.status(200).send(result);
       })
       .catch(() => {
-        response.status(500).send({ error: 'Cannot load the database' });
+        response.status(500).send('Cannot load the database');
       });
   }
 
@@ -19,14 +18,13 @@ class StudentsController {
       response.status(500).send('Major parameter must be CS or SWE'); // Envoyer une réponse avec le statut 500 et le message d'erreur
       return;
     }
-    readDatabase(path.join(__dirname, '../../database.csv'))
+    readDatabase('./database.csv')
       .then((data) => {
-        console.log(path);
         const studentsInMajor = major === 'CS' ? data.csArray : data.sweArray;
         response.status(200).send(`List: ${studentsInMajor.join(', ')}`);
       })
       .catch(() => {
-        response.status(500).send({ error: 'Cannot load the database' }); // Envoyer une réponse avec le statut 500 et le message d'erreur
+        response.status(500).send('Cannot load the database'); // Envoyer une réponse avec le statut 500 et le message d'erreur
       });
   }
 }
